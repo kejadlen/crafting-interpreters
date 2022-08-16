a944813d0d3807bbab073e11b14723090542c5d3
Alpha Chen <alpha@kejadlen.dev>
2022-08-16 16:08:32 -0700

    10.4

---
+++ ruby/lib/lox/function.rb
@@ -0,0 +1,23 @@
+require_relative "environment"
+
+module Lox
+  class Function
+    def initialize(decl)
+      @decl = decl
+    end
+
+    def arity = @decl.params.size
+
+    def call(interpreter, args)
+      env = Environment.new(interpreter.globals)
+      @decl.params.map(&:lexeme).zip(args).each do |name, value|
+        env.define(name, value)
+      end
+
+      interpreter.execute_block(@decl.body, env)
+      nil
+    end
+
+    def to_s = "<fn #{@decl.name.lexeme}>"
+  end
+end
