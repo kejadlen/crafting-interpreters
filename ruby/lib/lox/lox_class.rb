585003668918f93a06c0bf2d95e1d03b98487e2d
Alpha Chen <alpha@kejadlen.dev>
2022-08-30 10:08:33 -0700

    12.7

--- ruby/lib/lox/lox_class.rb
+++ ruby/lib/lox/lox_class.rb
@@ -11,8 +11,21 @@ module Lox
 
     def find_method(name) = @methods[name]
     def to_s = name
-    def call(interpreter, args) = Instance.new(self)
-    def arity = 0
+
+    def call(interpreter, args)
+      instance = Instance.new(self)
+
+      if init = find_method("init")
+        init.bind(instance).call(interpreter, args)
+      end
+
+      instance
+    end
+
+    def arity
+      init = find_method("init")
+      init ? init.arity : 0
+    end
 
   end
 end
